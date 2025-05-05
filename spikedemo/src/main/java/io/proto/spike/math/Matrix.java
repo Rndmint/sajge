437d05967436fea5b0fa1a72207950a55971b33f
Rndmint <integerrandom40@gmail.com>
2025-05-05 14:34:13 +0200

    spike(math): thinking...

---
+++ spikedemo/src/main/java/io/proto/spike/math/Matrix.java
@@ -0,0 +1,14 @@
+package io.proto.spike.math;
+
+public interface Matrix<T extends Matrix<T>> {
+    T add(T other);
+    T sub(T other);
+    T scale(float scalar);
+    T multiply(T other);
+
+    T transpose();
+    float determinant();
+    T inverse();
+
+    void setIdentity();
+}
