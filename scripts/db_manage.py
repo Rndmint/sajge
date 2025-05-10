0174afd107339db731bbd1f672908fd424580254
Rndmint <integerrandom40@gmail.com>
2025-05-10 02:40:02 +0200

    Tweaking the working directory because of pom issues

---
+++ scripts/db_manage.py
@@ -0,0 +1,7 @@
+import os, argparse, paramiko
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/deploy_server.py
@@ -0,0 +1,7 @@
+import argparse, os, paramiko
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
